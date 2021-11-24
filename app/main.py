e8a8c0412136ab5d1cb13d767407ee296f09654a
Daniel Nduati <dannduati2@gmail.com>
2021-11-24 03:43:03 +0300

    Update main.py
    Add Pyscopg usage which is a postgresql adapter for python. Use the
    adapter to connect to my postgress database and perform the same CRUD
    operations to our post data as before

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI,Response,status,HTTPException
 from fastapi.params import Body
 from typing import Optional
 from pydantic import BaseModel
+import app.config as my_config
+import psycopg2
+from psycopg2.extras import RealDictCursor
+import time
 
 # aplication instance
 app = FastAPI()
@@ -11,91 +15,81 @@ class Post(BaseModel):
     title: str
     content: str
     published: bool = True
-    rating: Optional[int] = None
+    
 
-#dummy in memory posts
-posts = [{
-    "title": "title of post 1",
-    "content": "content of post 1",
-    "id": 1,
-    "published": True,
-    "rating": 8
-    },
-    {
-    "title": "title of post 2",
-    "content": "content of post 2",
-    "id": 2,
-    "published": True,
-    "rating": 6.5 
-    }
-]
-
-def find_post(id):
-    for i,post in enumerate(posts):
-        if post["id"] == id:
-            return post
+#connect to postgress server instance and database
+while True:
+    try:
+        conn = psycopg2.connect(host=my_config.database_host,dbname=my_config.database_name,user=my_config.database_user,password=my_config.database_password,cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("Connection to database successful!")
+        break
+    except Exception as e:
+        print("Unable to connect to database!")
+        print("Error: ",e)
+        time.sleep(1)
 
 #path operations - synonymous to routes
-
 #root 
 @app.get("/")
-def root():
+async def root():
     message = "Hello am learning FastAPI!!"
     return{"message":f"{message}"}
 
 # get endpoint to get all posts
 @app.get("/posts")
 def get_posts():
-    return {"data":f"{posts}"}
+    cursor.execute("""SELECT * FROM posts""")
+    posts = cursor.fetchall()
+    #print(posts)
+    return {"data":posts}
 
 # post endpoint to add a post
 @app.post("/posts",status_code=status.HTTP_201_CREATED)
-def create_posts(post:Post): 
-    last_id = int(posts[-1]['id'])
-    #print(last_id)
-    payload = post.dict()
-    payload["id"] = last_id+1
-    #print(payload)
-    posts.append(payload)
-    return {"data":f"{payload}"}
+def create_posts(post:Post):
+    cursor.execute("""INSERT INTO posts (title,content,published) VALUES (%s,%s,%s) RETURNING * """,(post.title,post.content,post.published))
+    new_post = cursor.fetchone()
+    conn.commit()
+    return {"data":new_post}
 
 # get endpoint to get the latest post
 @app.get("/posts/latest")
 def get_latest():
+    cursor.execute("""SELECT * FROM posts ORDER BY id DESC LIMIT 1""")
+    posts = cursor.fetchall()
     post = posts[len(posts)-1]
-    return {"data":f"{post}"}
+    return {"data":post}
 
 
 # get endpoint to get a post by id as a path parameter
 @app.get("/posts/{post_id}")
 def get_post(post_id: int, response: Response):
-    post = find_post(post_id)
-    if not post:
-        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-        detail=f"post with id: {post_id} was not found")
-        #response.status_code = status.HTTP_404_NOT_FOUND
-        #return {"message": }
-    return {"data":post}
+    cursor.execute(F"""SELECT * FROM posts WHERE id= {str(post_id)}""")
+    test_post = cursor.fetchone()
+    if not test_post:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id {post_id} was not found")
+    return {"data":test_post}
 
 # post end point to delete a post
 @app.delete("/posts/{post_id}",status_code=status.HTTP_204_NO_CONTENT)
 def delete_post(post_id:int):
-    post = find_post(post_id)
+    cursor.execute(F"""SELECT * FROM posts WHERE id={str(post_id)}""")
+    post = cursor.fetchone()
     if not post:
         raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id: {post_id} was not found"))
     else:
-        # delete the post
-        posts.pop(post_id-1)
+        #delete post
+        cursor.execute(F"""DELETE FROM posts WHERE id={str(post_id)}""")
         return Response(status_code=status.HTTP_204_NO_CONTENT)
 
 # put endpoint to update a post
 @app.put("/posts/{post_id}")
 def update_post(post_id:int,post:Post):
-    if not find_post(post_id):
+    cursor.execute(F"""SELECT * FROM posts WHERE id={str(post_id)}""")
+    update_post = cursor.fetchone()
+    if not update_post:
         raise(HTTPException(status_code=status.HTTP_404_NOT_FOUND,detail=f"post with id: {post_id} was not found"))
     else:
-        #update the post
-        for key,value in post.dict().items():
-            #print(f"{key} : {value}")
-            posts[post_id-1][key] = value
-        return{"data":f"{posts[post_id-1]}","message":f"Successfully updated post wihth id {post_id}"}
+        cursor.execute(F"""UPDATE posts SET title={post.title}, content={post.content}, published={post.published} WHERE id={str(post_id)} RETURNING *""")
+        updated_post = cursor.fetchone()
+        return{"data":update_post,"message":f"Successfully updated post wihth id {post_id}"}
