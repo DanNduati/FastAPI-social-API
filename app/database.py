6a822d488264f3b879db68a0851957baab12475f
Daniel Nduati <dannduati2@gmail.com>
2021-11-25 02:36:10 +0300

    Add database.py
    Database script to connect to our postgress database. Define a
    sqlalchemy engine and create db session classes to be used on the main
    file when creating database instances during CRUD operations. A Base
    class is also created that uses the function declarative_base() and
    which will be inherited in database models

---
+++ app/database.py
@@ -0,0 +1,20 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import app.config as my_config
+
+SQLALCHEMY_DATABASE_URL = F"postgresql://{my_config.database_user}:{my_config.database_password}@{my_config.database_host}/{my_config.database_name}"
+
+#create sqlalchemy engine
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+#SessionLocal class -> instances of this class are database sessions
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+#Base class -> To be inherited when creating database ORM models
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
