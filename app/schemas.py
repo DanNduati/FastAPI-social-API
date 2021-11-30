3d0e31fd728a2b645d4b752c98d734e3346c950b
Daniel Nduati <dannduati2@gmail.com>
2021-11-30 14:01:02 +0300

    Update schemas.py
    Create the request and response pydantic models for user related path
    operations

--- app/schemas.py
+++ app/schemas.py
@@ -1,6 +1,9 @@
-from pydantic import BaseModel
+from pydantic import BaseModel,EmailStr
 from datetime import datetime
+from sqlalchemy import orm
+from app.models import User
 
+#posts schemas
 # request body pydantic model
 class PostBase(BaseModel):
     title: str
@@ -13,6 +16,21 @@ class PostCreate(PostBase):
 #response pydantic model
 class PostResponse(PostBase):
     id: int
+    created_at: datetime
+    class Config:
+        # Pydantic's orm_mode will tell the pydantic model to 
+        # read the data even if it is not a dict but an ORM model 
+        # or any other arbitrary object with attributes
+        orm_mode = True
+
+#users schemas
+class UserCreate(BaseModel):
+    email: EmailStr
+    password: str
+
+class UserResponse(BaseModel):
+    id: int
+    email: EmailStr
     created_at: datetime
     class Config:
         orm_mode = True
