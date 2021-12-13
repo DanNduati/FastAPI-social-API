724a196d6e4120da318f91d93b4d99b14db73798
Daniel Nduati <dannduati2@gmail.com>
2021-12-14 01:39:52 +0300

    Update the authentication router
    Use fastapi security utility `OAuth2PasswordRequestForm`. As specified in OAuth2 "password flow"
    that the client/user must send a username and password fields as form data not JSON.
    
    Update the path operation and use `OAuth2PasswordRequestForm` as a
    dependency in the path operation for `/login`
    
    Update the db query since the request form body will contain the
    username and password.

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter,status,HTTPException
 from fastapi.params import Depends
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 from ..database import get_db
 from .. import schemas,models,utils,oauth2
@@ -9,8 +10,8 @@ router = APIRouter(
 )
 
 @router.post("/login")
-async def login(user_credentials:schemas.UserLogin, db : Session = Depends(get_db)):
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+async def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db : Session = Depends(get_db)):
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid user credentials")
     if not utils.verify(user_credentials.password,user.password):
