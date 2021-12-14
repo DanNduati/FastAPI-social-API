b678808c93b089580228102db632bce2e2e0f810
Daniel Nduati <dannduati2@gmail.com>
2021-12-14 03:59:47 +0300

    Update oauth2.py
    Return current user by querying db for decoded user id from the jwt
    token payload data

--- app/oauth2.py
+++ app/oauth2.py
@@ -5,7 +5,9 @@ from fastapi.security import OAuth2PasswordBearer
 from jose import jwt
 from datetime import datetime,timedelta
 from jose.exceptions import JWTError
-from . import schemas
+from sqlalchemy.sql.functions import mode
+from . import schemas,database,models
+from sqlalchemy.orm import Session
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl="login")
 
@@ -34,6 +36,8 @@ def verify_access_token(token: str,credentials_exception):
         raise credentials_exception
     return token_data
 
-def get_current_user(token:str = Depends(oauth2_scheme)):
+def get_current_user(token:str = Depends(oauth2_scheme),db: Session = Depends(database.get_db)):
     credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED,detail="Could not validate credentials", headers={"WWW-Authenticate":"Bearer"})
-    return verify_access_token(token,credentials_exception)
+    token = verify_access_token(token,credentials_exception)
+    user = db.query(models.User).filter(models.User.id == token.id).first()
+    return user
