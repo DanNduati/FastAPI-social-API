de2312bd50bbad1afc5dc7f2b2911c4efae0b549
Daniel Nduati <dannduati2@gmail.com>
2021-12-19 01:35:38 +0300

    Update main.py
    Import and include the votes router

--- app/main.py
+++ app/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from app.routers import auth
 from . import models
 from .database import engine
-from .routers import posts, users, auth
+from .routers import posts, users, auth, votes
 
 #create our posts table if its not present
 models.Base.metadata.create_all(bind=engine)
@@ -15,6 +15,7 @@ app = FastAPI()
 app.include_router(posts.router)
 app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(votes.router)
 
 #root 
 @app.get("/")
