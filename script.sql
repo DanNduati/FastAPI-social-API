f19be102815e4e06c596a69c508f90cb0185ea56
Daniel Nduati <dannduati2@gmail.com>
2021-12-19 14:53:59 +0300

    Update sql join practice script
    Combine the left join and group by to get total votes on a post
    Order the above query by no. of votes

--- script.sql
+++ script.sql
@@ -1,4 +1,8 @@
 /*Joins practice*/
 select users.id,count(*) from posts right join users on posts.user_id = users.id group by users.id;
 select users.id,users.email,count(posts.id) as user_post_count from posts right join users on posts.user_id = users.id group by users.id;
-select posts.id as post_id, votes.* from posts left join votes on posts.id = votes.post_id
+select posts.id as post_id, votes.* from posts left join votes on posts.id = votes.post_id;
+/* count the total number of votes for each post*/
+select posts.id, count(votes.post_id) from posts left join votes on posts.id = votes.post_id group by posts.id;
+/*order*/
+select posts.id as post_id, count(votes.post_id) as vote_count from posts left join votes on posts.id = votes.post_id group by posts.id order by(vote_count) desc;
