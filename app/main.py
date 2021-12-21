3fda881fa00978dc446ce28885dd820be026bf0b
Daniel Nduati <dannduati2@gmail.com>
2021-12-21 17:16:38 +0300

    Update main module
    Use the CORSMiddleware to configure cross origin resource sharing

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import auth
 from . import models
 from .database import engine
@@ -10,6 +11,15 @@ models.Base.metadata.create_all(bind=engine)
 # aplication instance
 app = FastAPI()
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 #path operations - synonymous to routes
 #routers
 app.include_router(posts.router)
