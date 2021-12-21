da61a6a7dcdb01bab3472086e62c2c41fcf55e7c
Daniel Nduati <dannduati2@gmail.com>
2021-12-21 14:18:37 +0300

    Add test migration script
    ```alembic revision -m "migrations test"```

---
+++ alembic/versions/cdc9116b307a_migration_test.py
@@ -0,0 +1,24 @@
+"""migration test
+
+Revision ID: cdc9116b307a
+Revises: 
+Create Date: 2021-12-21 14:17:51.627237
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'cdc9116b307a'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade():
+    pass
+
+
+def downgrade():
+    pass
