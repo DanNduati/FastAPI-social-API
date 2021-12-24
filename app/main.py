10b53f06a8528cbdda4deaaa072e2854dfdc657e
Daniel Nduati <dannduati2@gmail.com>
2021-12-24 12:30:17 +0300

    Update main module router order

--- app/main.py
+++ app/main.py
@@ -33,7 +33,7 @@ app.add_middleware(
 )
 #path operations - synonymous to routes
 #routers
-app.include_router(posts.router)
-app.include_router(users.router)
 app.include_router(auth.router)
+app.include_router(users.router)
+app.include_router(posts.router)
 app.include_router(votes.router)
